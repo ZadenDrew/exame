31a9ee1ccf955fc895cd72a062bea9d09b5984c0
acabezaslopez <acabezaslopez@victoria15.danielcastelao.org>
2018-05-15 14:02:37 +0200

    examen

---
+++ src/examenaluguer/ExamenAluguer.java
@@ -0,0 +1,17 @@
+package examenaluguer;
+
+/**
+ *
+ * @author acabezaslopez
+ */
+public class ExamenAluguer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Aluguer al = new Aluguer();
+        al.setVisible(true);
+    }
+
+}
